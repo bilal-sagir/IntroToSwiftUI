f8431ce39229b8741e66946e3c95e416d230a7ff
bilal-sagir <sagir.bilal@icloud.com>
2022-04-02 20:13:49 +0300

    Initial Commit

---
+++ HelloState/HelloStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloStateApp.swift
+//  HelloState
+//
+//  Created by Bilal on 2.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct HelloStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
